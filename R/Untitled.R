5e00d6b222879d3cb08df6531cb3cb2bdbc1ffc0
Witold Wolski <wewolski@gmail.com>
2022-03-31 18:05:51 +0200

    copy script of signficant proteins.

---
+++ R/Untitled.R
@@ -0,0 +1,12 @@
+#' copy Markdown for 2 Grp.
+#' @param workdir directory where to copy file - default is current working directory.
+#' @keywords internal
+#' @export
+#'
+copy_2grp_markdown <- function(workdir = getwd()){
+  runscripts <- c("_Grp2Analysis.Rmd", "bibliography.bib", "DiffExpQC.Rmd", "MQ2GRPALLVSALL.R")
+  runscripts <- file.path("application/FragPipe2Grp/",runscripts )
+  prolfqua:::.scriptCopyHelperVec(runscripts, workdir = workdir, packagename = "prolfquapp")
+}
+
+
