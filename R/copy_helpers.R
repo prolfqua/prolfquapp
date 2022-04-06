def5a3a8f5c03c92d7dfb163eba39cf690296c42
Witold Wolski <wewolski@gmail.com>
2022-04-06 12:00:21 +0200

    Same markdown for MaxQuant.

--- R/copy_helpers.R
+++ R/copy_helpers.R
@@ -1,11 +1,21 @@
-#' copy Markdown for 2 Grp.
+#' copy Markdown and runscript for 2 Grp Fragpipe data.
 #' @param workdir directory where to copy file - default is current working directory.
 #' @keywords internal
 #' @export
 #'
-copy_2grp_markdown <- function(workdir = getwd()){
-  runscripts <- c("_Grp2Analysis.Rmd", "bibliography.bib", "_DiffExpQC.Rmd", "MQ2GRPALLVSALL.R")
-  runscripts <- file.path("application/FragPipe2Grp/",runscripts )
+copy_2grp_FragPipe <- function(workdir = getwd()){
+  runscripts <- c("application/_Grp2Analysis.Rmd", "application/bibliography.bib", "application/_DiffExpQC.Rmd", "application/FragPipe2Grp/FRG2GRP.R")
+  prolfqua:::.scriptCopyHelperVec(runscripts, workdir = workdir, packagename = "prolfquapp")
+}
+
+
+#' copy Markdown and runscript for 2 Grp Fragpipe data.
+#' @param workdir directory where to copy file - default is current working directory.
+#' @keywords internal
+#' @export
+#'
+copy_2grp_MaxQuant <- function(workdir = getwd()){
+  runscripts <- c("application/_Grp2Analysis.Rmd", "application/bibliography.bib", "application/_DiffExpQC.Rmd", "application/FragPipe2Grp/FRG2GRP.R")
   prolfqua:::.scriptCopyHelperVec(runscripts, workdir = workdir, packagename = "prolfquapp")
 }
 
