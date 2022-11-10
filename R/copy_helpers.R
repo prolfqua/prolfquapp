f8cafb2b3da94450bcc5fcfc013327748c1e80ed
Witold Wolski <wewolski@gmail.com>
2022-11-10 14:15:48 +0100

    commit

--- R/copy_helpers.R
+++ R/copy_helpers.R
@@ -3,7 +3,7 @@
 #' @keywords internal
 #' @export
 #'
-  copy_2grp_FragPipe <- function(workdir = getwd()){
+copy_2grp_FragPipe <- function(workdir = getwd()){
   runscripts <- c("application/_Grp2Analysis.Rmd", "application/bibliography.bib", "application/_DiffExpQC.Rmd", "application/FragPipe2Grp/FRG2GRP.R")
   prolfqua:::.scriptCopyHelperVec(runscripts, workdir = workdir, packagename = "prolfquapp")
 }
@@ -20,3 +20,16 @@ copy_2grp_MaxQuant <- function(workdir = getwd()){
 }
 
 
+#' copy Markdown and runscript for 2 Grp Fragpipe data.
+#' @param workdir directory where to copy file - default is current working directory.
+#' @keywords internal
+#' @export
+#'
+copy_DEA_FragPipe_TMT <- function(workdir = getwd()){
+  runscripts <- c("application/_Grp2Analysis.Rmd",
+                  "application/bibliography.bib",
+                  "application/_DiffExpQC.Rmd",
+                  "application/FragPipeTMT/FP_TMT.R")
+
+  prolfqua:::.scriptCopyHelperVec(runscripts, workdir = workdir, packagename = "prolfquapp")
+}
