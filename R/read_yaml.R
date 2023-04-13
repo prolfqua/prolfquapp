9e2a2c3d0502a034ef516e4da503486dbbd3cc3a
Witold Wolski <wewolski@gmail.com>
2023-04-13 12:08:25 +0200

    add function to create config.

--- R/read_yaml.R
+++ R/read_yaml.R
@@ -41,3 +41,58 @@ read_yaml <- function(ymlfile, application = "FragPipeTMT" ) {
   GRP2$zipdir <- ZIPDIR
   return(GRP2)
 }
+
+#' create GRP2 configuration.
+#' Use this function if there is no Yaml Input.
+#' @param patternDecoys default "REV_"
+#' @param patternContaminants default "zz_"
+#' @export
+#' @examples
+#' DEAconfig <- make_DEA_config()
+#' DEAconfig
+make_DEA_config <- function(
+    ZIPDIR  = ".",
+    PROJECTID = "",
+    ORDERID ="",
+    WORKUNITID ="",
+    Normalization = c("vsn", "quantile", "robscale"),
+    aggregation = c("medpolish" , "top3", "lmrob"),
+    Diffthreshold = 1,
+    FDRthreshold = 0.1,
+    removeContaminants = FALSE,
+    removeDecoys = FALSE,
+    patternDecoys = "REV_",
+    patternContaminants = "zz",
+    application = "FragPipeTMT" ){
+
+  Normalization <- match.arg(Normalization)
+  aggregation <- match.arg(aggregation)
+
+
+  GRP2 <- list()
+  GRP2$Bfabric <- list()
+  GRP2$Bfabric$projectID <- PROJECTID
+  GRP2$Bfabric$projectName <- "" # workunit name in the future.
+  GRP2$Bfabric$orderID <- ORDERID
+  GRP2$Bfabric$workunitID <- WORKUNITID
+  GRP2$Bfabric$inputID <- NULL
+  GRP2$Bfabric$inputURL <- NULL
+
+  #at least 2 peptides per protein
+  GRP2$pop <- list()
+  GRP2$pop$transform <- Normalization
+
+  GRP2$pop$aggregate <- aggregation
+  GRP2$pop$Diffthreshold <- Diffthreshold
+  GRP2$pop$FDRthreshold <- FDRthreshold
+
+  GRP2$pop$removeCon <- removeContaminants
+  GRP2$pop$removeDecoys <- removeDecoys
+
+  GRP2$pop$revpattern <- patternDecoys
+  GRP2$pop$contpattern <- patternContaminants
+
+  GRP2$Software <- application
+  GRP2$zipdir <- ZIPDIR
+  return(GRP2)
+}
