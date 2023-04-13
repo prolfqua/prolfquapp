6ddfc20899910696b4112e2d1ecc3a122080a521
Witold Wolski <wewolski@gmail.com>
2023-04-13 16:53:41 +0200

    set factors function

--- R/dataset.R
+++ R/dataset.R
@@ -27,6 +27,24 @@ dataset_extract_contrasts <- function(annot, GRP2) {
 
 #' set factors and sample names columns
 #' @export
+#' @examples
+#' dataset_csv_examples <- system.file("application/dataset_csv",package = "prolfquapp")
+#' files <- dir(dataset_csv_examples, pattern = "*.csv")
+#'
+#' res <- list()
+#' for (i in seq_along(files)) {
+#'   print(i)
+#'   res[[i]] <- readr::read_csv(files[i])
+#' }
+#'
+#' for (i in seq_along(res)) {
+#'   atable <- prolfqua::AnalysisTableAnnotation$new()
+#'   atable$fileName = "channel"
+#'   atable$hierarchy[["protein_Id"]] <- c("Protein")
+#'   atable$hierarchy[["peptide_Id"]] <- c("Peptide")
+#'   tmp <- prolfquapp::dataset_set_factors(atable, res[[i]] )
+#'   cat(i, " : " , length(tmp$atable$factors), "factors : ", paste(tmp$atable$factors, collapse = "; "), "\n")
+#' }
 #'
 dataset_set_factors <- function(atable, msdata, REPEATED = TRUE) {
   atable$hierarchyDepth <- 1
@@ -34,16 +52,16 @@ dataset_set_factors <- function(atable, msdata, REPEATED = TRUE) {
     atable$sampleName <- grep("^name", colnames(msdata), value = TRUE, ignore.case = TRUE)
   }
 
-  stopifnot(sum(grepl("^group|^bait", colnames(msdata), ignore.case = TRUE)) == 1)
-  groupingVAR <- grep("^group|^bait", colnames(msdata), value = TRUE, ignore.case = TRUE)
+  stopifnot(sum(grepl("^group|^bait|^Experiment", colnames(msdata), ignore.case = TRUE)) == 1)
+  groupingVAR <- grep("^group|^bait|^Experiment", colnames(msdata), value = TRUE, ignore.case = TRUE)
   msdata[[groupingVAR]] <- gsub("[[:space:]]", "", msdata[[groupingVAR]])
   msdata[[groupingVAR]] <- gsub("[-\\+\\/\\*]", "_", msdata[[groupingVAR]])
 
   atable$factors[["Group_"]] = groupingVAR
   atable$factorDepth <- 1
 
-  if (sum(grepl("^subject", colnames(msdata), ignore.case = TRUE)) == 1 & REPEATED) {
-    subvar <- grep("^subject", colnames(msdata), value = TRUE, ignore.case = TRUE)
+  if (sum(grepl("^subject|^BioReplicate", colnames(msdata), ignore.case = TRUE)) == 1 & REPEATED) {
+    subvar <- grep("^subject|^BioReplicate", colnames(msdata), value = TRUE, ignore.case = TRUE)
     atable$factors[["Subject"]] = subvar
     tmp <- data.frame(table(dplyr::distinct(msdata[,c(groupingVAR,subvar)])) )
     if (all(tmp$Freq > 1)) {
