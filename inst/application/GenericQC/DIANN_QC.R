2eedc4b30b4c4f7254256c6ad204cc6c263d752c
Witold Wolski <wewolski@gmail.com>
2023-05-04 10:19:17 +0200

    log file paths

--- inst/application/GenericQC/DIANN_QC.R
+++ inst/application/GenericQC/DIANN_QC.R
@@ -54,11 +54,11 @@ logger::log_info(fasta.file)
 
 diann.output <- mdir(path,
                      pattern = "diann-output.tsv")
-logger::log_info(fasta.file)
+logger::log_info(diann.output)
 
 dataset.csv <- mdir(path,
                     pattern = "dataset.csv")
-logger::log_info(fasta.file)
+logger::log_info(dataset.csv)
 
 peptide <- read_DIANN_output(
   diann.path = diann.output,
