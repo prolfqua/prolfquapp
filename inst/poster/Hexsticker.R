b5c55e1b7539252c606007d38cfcbc21df3792de
Witold Wolski <wewolski@gmail.com>
2023-06-01 10:57:20 +0200

    prolfqua poster output

---
+++ inst/poster/Hexsticker.R
@@ -0,0 +1,19 @@
+library(hexSticker)
+imgurl = "4X43_Merge_APP.png"
+
+
+sticker(imgurl,
+        package = "prolfquapp",
+        p_color = "black",
+        p_size = 20,
+        #p_fontface = "italic",
+        p_y = 1.6,
+        s_x = 0.95,
+        s_y = 0.85,
+        s_width = 0.9,
+        h_fill = "cornflowerblue",
+        h_color = "darkblue",
+        filename = "hexstrickerPRLapp.png",
+        url	= "github.com/wolski/prolfquapp",
+        u_size = 5,
+        u_color = "black")
