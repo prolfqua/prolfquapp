941519f697c264c82078cd76547af698ba207f69
Witold Wolski <wolski@FGCZ-NET.unizh.ch>
2023-08-31 16:10:39 +0200

    In addition extract protein length.

--- R/get_annot_from_FASTA.R
+++ R/get_annot_from_FASTA.R
@@ -1,8 +1,15 @@
 #' get_annot_from_fasta
-#' @export
 #'
-get_annot_from_fasta <- function(fasta.files, rev= "REV_", isUniprot = TRUE){
-
+#' @export
+#' @examples
+#'
+#' prolfquapp::get_annot_from_fasta(fasta.files)
+#'
+#'
+get_annot_from_fasta <- function(
+    fasta.files,
+    rev= "REV_",
+    isUniprot = TRUE) {
   fasta <- list()
   for (fasta.file in fasta.files) {
     x <- seqinr::read.fasta(file = fasta.file, as.string = TRUE, seqtype = "AA")
@@ -12,11 +19,14 @@ get_annot_from_fasta <- function(fasta.files, rev= "REV_", isUniprot = TRUE){
   fasta_annot <- prolfqua::matrix_to_tibble(
     data.frame(annot = sapply(fasta, seqinr::getAnnot)), preserve_row_names = NULL
   )
+  fasta_annot$protein_length <- vapply(fasta, nchar, 0)
+
   fasta_annot <- fasta_annot |> tidyr::separate(.data$annot,
                                                 c("fasta.id","fasta.header"),
                                                 sep = "\\s", extra = "merge")
   fasta_annot <- fasta_annot |> dplyr::mutate(fasta.id = gsub(">","", .data$fasta.id) )
   fasta_annot <- fasta_annot[!grepl(rev,fasta_annot$fasta.id), ]
+
 
   if (isUniprot) {
     fasta_annot <- fasta_annot |> dplyr::mutate(proteinname = gsub(".+\\|(.+)\\|.*","\\1", .data$fasta.id) )
