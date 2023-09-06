1280d8f2d6dc3df5a058a821d2299fb6794f947b
Witold Wolski <wolski@FGCZ-NET.unizh.ch>
2023-09-06 10:08:36 +0200

    copying bib files to working directory funciton

--- R/copy_helpers.R
+++ R/copy_helpers.R
@@ -55,3 +55,17 @@ copy_DEA_DIANN <- function(workdir = getwd(), run_script = FALSE) {
   prolfqua:::.scriptCopyHelperVec(runscripts, workdir = workdir, packagename = "prolfquapp")
 }
 
+
+#' copy Markdown and runscript for DIANN diann-output.tsv
+#' @param workdir directory where to copy file - default is current working directory.
+#' @export
+#'
+copy_SAINT_express <- function(workdir = getwd(), run_script = FALSE) {
+  runscripts <- c(
+    "application/bibliography.bib",
+    "application/SE2/SaintExpressReportMsFragger.Rmd",
+    if (run_script) {"application/SE2/CreateSaintExpress_Report.R"},
+    if (run_script) {"application/SE2_DIANN/DIANN_SE.R"}
+  )
+  prolfqua:::.scriptCopyHelperVec(runscripts, workdir = workdir, packagename = "prolfquapp")
+}
