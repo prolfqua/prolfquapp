9b1b5afd32d9ff4ab8936fc141be52debf52c5a6
Grossmann <jg@fgcz.ethz.ch>
2024-04-09 11:36:42 +0200

    Update preprocess_FP_PSM.R
    use tidy_FP_psm from prolfquapp not from prolfqua

--- R/preprocess_FP_PSM.R
+++ R/preprocess_FP_PSM.R
@@ -32,7 +32,7 @@ preprocess_FP_PSM <- function(quant_data,
                     (basename(annot[[atable$fileName]]))
     ))
 
-  psm <- prolfqua::tidy_FragPipe_psm(quant_data, column_before_quants)
+  psm <- prolfquapp::tidy_FragPipe_psm(quant_data, column_before_quants)
   psm$qValue <- 1 - psm$PeptideProphet.Probability
 
   nrowPSM <- nrow(psm)
