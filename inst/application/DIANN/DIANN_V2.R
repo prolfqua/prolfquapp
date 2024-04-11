40ef9d22996ad4d881ced12bede465c1f809bc1d
Witold Wolski <wewolski@gmail.com>
2024-04-11 10:21:56 +0200

    input folder

--- inst/application/DIANN/DIANN_V2.R
+++ inst/application/DIANN/DIANN_V2.R
@@ -5,24 +5,18 @@ path = "."
 ymlfile <- file.path(path,"config.yaml")
 GRP2 <- prolfquapp::read_BF_yamlR6(ymlfile, application = "DIANN")
 
-
-dir.create(GRP2$zipdir)
-inputs <- file.path(GRP2$zipdir,
-paste0("Inputs_WU", GRP2$project_spec$workunitID))
-dir.create(inputs)
-
 dsf = file.path(path,"dataset.csv")
 dsf <- readr::read_csv(dsf)
 annotation <- prolfquapp::read_annotation(dsf)
-
-annotation$atable$fileName
-
 files <- prolfquapp::get_DIANN_files(path)
 
-xd <- prolfquapp::preprocess_DIANN(quant_data = files$data,
-                                   fasta_file = files$fasta,
-                                   annotation = annotation)
-
+undebug(prolfquapp::preprocess_DIANN)
+xd <- prolfquapp::preprocess_DIANN(
+  quant_data = files$data,
+  fasta_file = files$fasta,
+  annotation = annotation,
+  nrPeptides = 1,
+  q_value = 0.01)
 
 
 logger::log_info("AGGREGATING PEPTIDE DATA!")
@@ -33,6 +27,7 @@ logger::log_info("END OF PROTEIN AGGREGATION")
 logger::log_info("run analysis")
 grp <- prolfquapp::generate_DEA_reports2(lfqdata, GRP2, xd$protein_annotation, annotation$contrasts)
 
+
 logger::log_info("write results and html reports")
 prolfquapp::write_DEA_all(grp[[1]], names(grp)[1], GRP2$zipdir , boxplot = FALSE, markdown = "_Grp2Analysis_V2.Rmd")
 
@@ -42,5 +37,15 @@ SE <- prolfquapp::make_SummarizedExperiment(grp[[1]])
 saveRDS(SE, file = file.path(GRP2$zipdir,
                              paste0("Results_DEA_WU", grp[[1]]$project_spec$workunitID) ,
                              paste0("SummarizedExperiment",".rds") ))
+
+
+### put all inputs into indir
+
+inputs <- file.path(GRP2$zipdir,
+                    paste0("Inputs_DEA_WU", GRP2$project_spec$workunitID))
+dir.create(inputs)
+prolfquapp::copy_DEA_DIANN(workdir = inputs, run_script = TRUE)
+file.copy(files$data, inputs)
+file.copy(files$fasta, inputs)
 
 
