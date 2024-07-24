e4c9a3c1bdd8e39711bea4359b1c71d5b0e05287
Witold Wolski <wewolski@gmail.com>
2024-07-24 08:29:08 +0200

    log reading fasta file

--- R/preprocess_MSstats.R
+++ R/preprocess_MSstats.R
@@ -81,8 +81,10 @@ preprocess_MSstats <- function(quant_data,
   config <- prolfqua::AnalysisConfiguration$new(atable)
   adata <- prolfqua::setup_analysis(apeptide, config)
   lfqdata <- prolfqua::LFQData$new(adata, config)
-
+  logger::log_info("Start reading fasta: ", fasta_file )
   fasta_annot <- get_annot_from_fasta(fasta_file,rev = pattern_decoys)
+  logger::log_info("Finished reading fasta: ", fasta_file )
+
   fasta_annot <- dplyr::left_join(nrPeptides_exp, fasta_annot, by = c("ProteinName" = "fasta.id"))
 
   fasta_annot <- fasta_annot |> dplyr::rename(!!lfqdata$config$table$hierarchy_keys_depth()[1] := !!rlang::sym("ProteinName"))
