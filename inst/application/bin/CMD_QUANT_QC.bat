4c91832abb17afb63af2079cc326cf33ef36905a
Witold Wolski <wewolski@gmail.com>
2024-08-19 15:47:01 +0200

    make bat

--- inst/application/bin/CMD_QUANT_QC.bat
+++ inst/application/bin/CMD_QUANT_QC.bat
@@ -1,16 +1,18 @@
-#!/bin/bash
+@echo off
 
-# Get the path to the installed R package
-PACKAGE_PATH=$(Rscript --vanilla -e "cat(system.file(package = 'prolfquapp'))")
+REM Get the path to the installed R package
+for /f "tokens=*" %%i in ('Rscript --vanilla -e "cat(system.file(package = 'prolfquapp'))"') do set PACKAGE_PATH=%%i
 
-# Build the path to the R script
-R_SCRIPT_PATH="${PACKAGE_PATH}/application/CMD_QUANT_QC.R"
+REM Build the path to the R script
+set R_SCRIPT_PATH=%PACKAGE_PATH%\application\CMD_QUANT_QC.R
 
-# Check if the R script exists
-if [[ -f "$R_SCRIPT_PATH" ]]; then
-    echo "Rscript --vanilla \"$R_SCRIPT_PATH\" \"$@\""
-    Rscript --vanilla "$R_SCRIPT_PATH" "$@"
-else
-    echo "Error: R script not found at '$R_SCRIPT_PATH'"
-    exit 1
-fi
+REM Check if the R script exists
+if exist "%R_SCRIPT_PATH%" (
+    Rscript --vanilla "%R_SCRIPT_PATH%" %*
+) else (
+    echo Error: R script not found at '%R_SCRIPT_PATH%'
+    exit /b 1
+)
+
+
+
