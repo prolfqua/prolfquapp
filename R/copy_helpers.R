16bb610576e55e09a342a6c46c2941f42525b35e
Witold Wolski <wewolski@gmail.com>
2024-08-19 15:42:33 +0200

    adding bat files

--- R/copy_helpers.R
+++ R/copy_helpers.R
@@ -5,10 +5,23 @@
 copy_shell_script <- function(workdir = getwd()) {
   runscripts <- c(
     "application/bin/CMD_DEA.sh",
-    "application/bin/CMD_DEA.bat",
     "application/bin/CMD_MAKE_YAML.sh",
     "application/bin/CMD_QUANT_QC.sh",
     "application/bin/CMD_MAKE_DATASET.sh"
+  )
+  prolfqua::scriptCopyHelperVec(runscripts, workdir = workdir, packagename = "prolfquapp")
+}
+
+#' copy shellscript to run the DEA app
+#' @param workdir directory where to copy file - default is current working directory.
+#' @export
+#'
+copy_bat_files <- function(workdir = getwd()) {
+  runscripts <- c(
+    "application/bin/CMD_DEA.bat",
+    "application/bin/CMD_MAKE_YAML.bat",
+    "application/bin/CMD_QUANT_QC.bat",
+    "application/bin/CMD_MAKE_DATASET.bat"
   )
   prolfqua::scriptCopyHelperVec(runscripts, workdir = workdir, packagename = "prolfquapp")
 }
---
+++ inst/application/bin/CMD_QUANT_QC.bat
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get the path to the installed R package
+PACKAGE_PATH=$(Rscript --vanilla -e "cat(system.file(package = 'prolfquapp'))")
+
+# Build the path to the R script
+R_SCRIPT_PATH="${PACKAGE_PATH}/application/CMD_QUANT_QC.R"
+
+# Check if the R script exists
+if [[ -f "$R_SCRIPT_PATH" ]]; then
+    echo "Rscript --vanilla \"$R_SCRIPT_PATH\" \"$@\""
+    Rscript --vanilla "$R_SCRIPT_PATH" "$@"
+else
+    echo "Error: R script not found at '$R_SCRIPT_PATH'"
+    exit 1
+fi
