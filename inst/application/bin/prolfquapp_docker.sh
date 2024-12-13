58f7c21c318066ab6b0a9d9c2afd7e8a707bda0a
Witold Wolski <wewolski@gmail.com>
2024-12-13 09:49:04 +0100

    Update prolfquapp_docker.sh

--- inst/application/bin/prolfquapp_docker.sh
+++ inst/application/bin/prolfquapp_docker.sh
@@ -8,7 +8,7 @@
 set -euo pipefail
 
 # Default values for arguments
-IMAGE_VERSION="0.0.5"
+IMAGE_VERSION="0.0.8"
 IMAGE_REPO="docker.io/prolfqua/prolfquapp"
 
 # Function to print usage/help message
