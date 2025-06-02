d2b3aa278fa7a296d84c9259cd48d63f56c23ec8
Witold Wolski <wewolski@gmail.com>
2025-06-02 14:59:31 +0200

    Update CMD_QUANT_QC.R

--- inst/application/CMD_QUANT_QC.R
+++ inst/application/CMD_QUANT_QC.R
@@ -172,7 +172,7 @@ pap <- QC_generator$new(xd$lfqdata, xd$protein_annotation, GRP2)
 # dd <- pap$get_prot_wide()
 # pap$get_prot_IBAQ_wide()
 
-# wirte parquet
+# write parquet
 pap$write_xlsx()
 
 #library(arrow)
